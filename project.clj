fd870301dd0a34bb468a98d5b7e6a7f328a88721
nmburgan <13688219+nmburgan@users.noreply.github.com>
2025-12-30 16:29:20 -0800

    Use released clj-parent version

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
 
   :min-lein-version "2.9.1"
 
-  :parent-project {:coords [org.openvoxproject/clj-parent "7.4.1-SNAPSHOT"]
+  :parent-project {:coords [org.openvoxproject/clj-parent "7.5.0"]
                    :inherit [:managed-dependencies]}
 
   ;; Abort when version ranges or version conflicts are detected in
