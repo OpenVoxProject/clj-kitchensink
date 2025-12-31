b476d0ae021970cfee4f9bbf6ce616d967a59e01
OpenVoxProjectBot <215568489+OpenVoxProjectBot@users.noreply.github.com>
2025-12-31 00:46:38 +0000

    Version 3.4.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.openvoxproject/kitchensink "3.4.2-SNAPSHOT"
+(defproject org.openvoxproject/kitchensink "3.4.2"
   :description "Clojure utility functions"
   :url "https://github.com/openvoxproject/clj-kitchensink"
   :license {:name "Apache License, Version 2.0"
